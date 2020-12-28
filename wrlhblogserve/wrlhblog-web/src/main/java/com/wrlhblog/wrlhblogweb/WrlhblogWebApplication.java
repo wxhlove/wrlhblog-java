b84454117c90befea9d233b081642844a512c0f2
wuxinhua <601983393@qq.com>
2020-12-28 13:51:41 +0800

    blog 初始化

---
+++ wrlhblogserve/wrlhblog-web/src/main/java/com/wrlhblog/wrlhblogweb/WrlhblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.wrlhblog.wrlhblogweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrlhblogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrlhblogWebApplication.class, args);
+	}
+
+}
---
+++ wrlhblogserve/wrlhblog-web/src/test/java/com/wrlhblog/wrlhblogweb/WrlhblogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrlhblog.wrlhblogweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrlhblogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
